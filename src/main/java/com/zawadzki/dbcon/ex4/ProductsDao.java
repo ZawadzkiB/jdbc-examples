bb8b48c0603ff31b894527a5465f0cdd532ad0f3
Zawadzki <zawadzkibartek@gmail.com>
2019-10-21 13:59:40 +0200

    sql objects

---
+++ src/main/java/com/zawadzki/dbcon/ex4/ProductsDao.java
@@ -0,0 +1,34 @@
+package com.zawadzki.dbcon.ex4;
+
+import com.zawadzki.dbcon.ex3.Product;
+import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@RegisterConstructorMapper(Product.class)
+public interface ProductsDao {
+
+  @SqlQuery("select * from products")
+  Stream<Product> getAll();
+
+  @SqlQuery("select * from products where id=:id")
+  Optional<Product> getById(@Bind("id") Integer id);
+
+  @SqlQuery("select * from products where name=:name")
+  Stream<Product> getAllByName(@Bind("name") String name);
+
+  @SqlQuery("select * from products where category=:category")
+  Stream<Product> getAllByCategory(@Bind("category") String category);
+
+  @SqlUpdate("INSERT INTO products(name, category, price) VALUES (:name, :category, :price)")
+  Boolean insertProduct(@BindBean Product product);
+
+  @SqlUpdate("update products set name=:name, category=:category, price=:price where id=:id")
+  Boolean updateproduct(@BindBean Product product);
+
+}
--- src/test/java/com/zawadzki/dbcon/ex4/CrudToDoTests.java
+++ src/test/java/com/zawadzki/dbcon/ex4/CrudToDoTests.java
@@ -1,5 +1,16 @@
 package com.zawadzki.dbcon.ex4;
 
+import com.zawadzki.dbcon.ex3.Product;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class CrudToDoTests {
 
   /**
@@ -14,4 +25,75 @@ public class CrudToDoTests {
    * 6. Insert Product
    * 7. Delete product
    */
+
+  private Jdbi jdbi;
+
+  @BeforeEach
+  void setupJDBI() {
+    jdbi = Jdbi.create(
+            "jdbc:postgresql://localhost:5432/example1",
+            "example1",
+            "example1");
+    jdbi.installPlugin(new SqlObjectPlugin());
+
+  }
+
+  @Test
+  void getAll(){
+    Stream<Product> productStream = jdbi.withExtension(ProductsDao.class, ProductsDao::getAll);
+    assertThat(productStream.count()).isEqualTo(9);
+  }
+
+  @Test
+  void getAllById() {
+    Product product = jdbi.withExtension(ProductsDao.class, it -> it.getById(1))
+            .orElse(new Product());
+
+    assertThat(product.getId()).isNotNull();
+  }
+
+  @Test
+  void getAllByName() {
+    Stream<Product> productStream = jdbi.withExtension(ProductsDao.class,
+            it -> it.getAllByName("bike"));
+
+    assertThat(productStream.allMatch(
+            it -> it.getName().equalsIgnoreCase("bike"))).isTrue();
+  }
+
+  @Test
+  void getAllByCategory() {
+    Stream<Product> productStream = jdbi.withExtension(ProductsDao.class,
+            it -> it.getAllByCategory("electronics"));
+
+    assertThat(productStream.allMatch(
+            it -> it.getCategory().equalsIgnoreCase("electronics"))).isTrue();
+  }
+
+  @Test
+  void updateProduct(){
+    Boolean updated = jdbi.withExtension(ProductsDao.class, it ->
+            it.updateproduct(
+                    new Product()
+                    .setName("update1")
+                    .setCategory("u1")
+                    .setPrice(BigDecimal.valueOf(111L))
+                    .setId(1)
+            ));
+
+    assertThat(updated).isTrue();
+  }
+
+  @Test
+  void insertProduct() {
+    Boolean inserted = jdbi.withExtension(ProductsDao.class, it -> it.insertProduct(
+            new Product()
+                    .setName("update1")
+                    .setCategory("u1")
+                    .setPrice(BigDecimal.valueOf(111L))
+                    .setId(1)
+    ));
+
+    assertThat(inserted).isTrue();
+  }
 }
